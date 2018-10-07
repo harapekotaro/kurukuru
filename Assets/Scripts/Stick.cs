44ac498b7afae1d53523346174c735c7bfaa7a25
harapekotaro <gakutoipad2012@gmail.co>
2018-10-08 08:45:21 +0900

    no message

--- Assets/Scripts/Stick.cs
+++ Assets/Scripts/Stick.cs
@@ -17,14 +17,44 @@ public class Stick : MonoBehaviour {
 	private bool butukari = false;
 	// 積算時間を収納
 	private float timeElapsed;
+	// 移動方向
 	private Vector2 direction;
-	
+	// ぶつかり法線
+	private Vector2 coldir;
 
 	void Start(){
 		// Rigidbody2Dを取得
 		rb2d = GetComponent<Rigidbody2D>();
 		// SpriteRendererの取得
 		spriter = gameObject.GetComponent<SpriteRenderer> ();
+	}
+
+		void FixedUpdate () {
+		// 移動
+		rb2d.velocity = direction * speed;
+		// ぶつかったときにぶつかりモードにはいる
+		if (butukari){
+			ButukariMode();				
+		}
+		else{
+			StickRotator();
+		}
+		//ダメージを受けた時の処理
+		if(butukari){
+			float level = Mathf.Abs(Mathf.Sin(Time.time * 10));
+			spriter.color =  new Color(28f,207f,151f,level);
+		}
+	}
+
+
+	// Update is called once per frame
+	void Update () {
+		// 入力を検出
+		float x = Input.GetAxisRaw("Horizontal");
+		float y = Input.GetAxisRaw("Vertical");
+		//方向を決定
+		direction = new Vector2(x, y).normalized;
+
 	}
 
 	// 棒操作
@@ -58,47 +88,26 @@ public class Stick : MonoBehaviour {
 	{
 		// ぶつかってからの時間を計測
 		timeElapsed += Time.deltaTime;
+		// 反発
+		if(timeElapsed <= 0.2f){
+		rb2d.AddForce(coldir * 4f, ForceMode2D.Impulse);
+		}
 		// timeElapsedが設定した時間を越えるとぶつかりモードを終了する
 		if(timeElapsed >= 0.7f) 
 		{
-			rb2d.angularVelocity = 0f;
 			timeElapsed = 0.0f;
 			butukari = false;
-			spriter.color =  new Color(28f,207f,151f,0.7f);
+			spriter.color =  new Color(28f,207f,151f,1f);
+			rb2d.angularVelocity = 0f;
 		}
 	}
 
-	void FixedUpdate () {
-		// 移動
-		rb2d.velocity = direction * speed;
-
-		//ダメージを受けた時の処理
-		if(butukari){
-			float level = Mathf.Abs(Mathf.Sin(Time.time * 10));
-			spriter.color =  new Color(28f,207f,151f,level);
-		}
-	}
-
-
-	// Update is called once per frame
-	void Update () {
-		// 入力を検出
-		float x = Input.GetAxisRaw("Horizontal");
-		float y = Input.GetAxisRaw("Vertical");
-		//方向を決定
-		direction = new Vector2(x, y).normalized;
-
-		// ぶつかったときにぶつかりモードにはいる
-		if (butukari){
-			ButukariMode();				
-		}
-		else{
-			StickRotator();
-		}
-	}
-
-	void OnCollisionStay2D(Collision2D c)
+	void OnCollisionEnter2D(Collision2D c)
 	{
-			butukari = true;
+		// ぶつかったとき接触した場所との法線を取得
+		if (!butukari){
+		coldir = c.contacts[0].normal;
+		}
+		butukari = true;
 	}
 }
